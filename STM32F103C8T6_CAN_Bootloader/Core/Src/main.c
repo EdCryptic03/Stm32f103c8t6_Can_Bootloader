c9b967b269527eb31995a544f8303293e77d2da3
Harshit Singh <harshit18sep008@gmail.com>
2026-09-08 00:01:27 +0200

    Wrote Self-Debugger Test via Hardware Debugger to verfiy FPEC unlock sequence etc

--- STM32F103C8T6_CAN_Bootloader/Core/Src/main.c
+++ STM32F103C8T6_CAN_Bootloader/Core/Src/main.c
@@ -1,8 +1,20 @@
 #include <stm32f1xx.h>
+#include "flash.h"
 
 #define APP_ADDRESS 0x08004400UL
 
+#define TEST_ADDR 0x0800FC00U
+
 typedef void (*app_entry_t)(void);
+
+// Declaring test global variables. I've used g in the naming so its easy to recognize
+volatile flash_status_t g_erase_status;
+volatile uint16_t g_after_erase;
+volatile flash_status_t g_prog_status;
+volatile uint16_t g_after_prog;
+volatile int g_flash_test_pass;
+
+
 
 static void jump_to_application(void){
 
@@ -18,8 +30,28 @@ static void jump_to_application(void){
 	app_entry();
 }
 
+/* Self-Debugger test function to verify FPEC unlock sequence, page erase,half-word program,
+ * re-lock, with an address guard that refuses the bootloader's own pages (0-15)
+ */
+static void flash_test(void){
+
+	flash_unlock();
+	g_erase_status = flash_erase_page(TEST_ADDR);
+	g_after_erase = *(volatile uint16_t *)TEST_ADDR;
+	g_prog_status = flash_haflword_program(TEST_ADDR, 0xABCD);
+	g_after_prog = *(volatile uint16_t *)TEST_ADDR;
+	flash_lock();
+
+	if(g_erase_status == FLASH_OK && g_after_erase == 0xFFFF && g_prog_status == FLASH_OK
+			&& g_after_prog == 0XABCD){
+		g_flash_test_pass = 1;
+	}
+}
+// _______________________________________________End of debugger function
+
 int main(void){
 
-	jump_to_application();
+	flash_test();
+//	jump_to_application();
 	while(1) { }
 }
