23610cbfe0d99eb5287320c0873790c3a1da8767
Harshit Singh <harshit18sep008@gmail.com>
2026-09-07 18:06:37 +0200

    Session 2: Created flash.h and flash.c

---
+++ STM32F103C8T6_CAN_Bootloader/Core/Inc/flash.h
@@ -0,0 +1,29 @@
+/*
+ * flash.h
+ *
+ *  Created on: 7 Sept 2026
+ *      Author: Harshit Singh
+ */
+
+#ifndef FLASH_H
+#define FLASH_H
+
+#include <stdint.h>
+
+typedef enum {
+
+	FLASH_OK = 0,
+	FLASH_ERR_RANGE,
+	FLASH_ERR_PROG,
+	FLASH_ERR_WRP
+} flash_status_t;
+
+void flash_unlock(void);
+void flash_lock(void);
+flash_status_t flash_erase_page(uint32_t page_addr);
+flash_status_t flash_halfword_program(uint32_t addr, uint16_t data);
+
+
+
+
+#endif /* INC_FLASH_H_ */
---
+++ STM32F103C8T6_CAN_Bootloader/Core/Src/flash.c
@@ -0,0 +1,87 @@
+/*
+ * flash.c
+ *
+ *  Created on: 7 Sept 2026
+ *      Author: Harshit Singh
+ */
+
+
+#include "flash.h"
+#include "stm32f1xx.h"
+
+// Keys for unlocking FPEC (FLash Program/Erase Controller)
+#define FLASH_KEY1 0x45670123U
+#define FLASH_KEY2 0xCDEF89ABU
+
+#define WRITABLE_START 0x08004000U
+#define FLASH_END_ADDR 0x08010000U
+
+static void flash_wait_busy(void){
+
+	while(FLASH->SR & FLASH_SR_BSY) {}
+}
+
+
+/* Functions for unlocking/locking FPEC using the flash control register
+ * and the magic keys
+ */
+void flash_unlock(void){
+
+	if(FLASH->CR & FLASH_CR_LOCK) {
+		FLASH->KEYR = FLASH_KEY1;
+		FLASH->KEYR = FLASH_KEY2;
+
+	}
+}
+
+void flash_lock(void){
+
+	FLASH->CR |= FLASH_CR_LOCK;
+}
+
+// End of the locking/unlocking functions
+// ____________________________________
+
+// Function for erasing page using reference manual procedure
+flash_status_t flash_erase_page(uint32_t page_addr){
+
+	if(page_addr <= WRITABLE_START || page_addr >= FLASH_END_ADDR){
+		return FLASH_ERR_RANGE;
+	}
+
+	flash_wait_busy();
+	FLASH->CR |= FLASH_CR_PER;
+	FLASH->AR = page_addr;
+	FLASH->CR |= FLASH_CR_STRT;
+	flash_wait_busy();
+	FLASH->CR &= ~FLASH_CR_PER;
+
+	if(FLASH->SR & FLASH_SR_WRPRTERR){
+		FLASH->SR = FLASH_SR_WRPRTERR;
+		return FLASH_ERR_WRP;
+	}
+
+	return FLASH_OK;
+}
+
+// Function for writing the 16 bit or half word data into the flash
+flash_status_t flash_halfword_program(uint32_t addr, uint16_t data){
+
+	if(addr <= WRITABLE_START || addr >= FLASH_END_ADDR){
+		return FLASH_ERR_RANGE;
+	}
+
+	flash_wait_busy();
+	FLASH->CR |= FLASH_CR_PG;
+	*(volatile uint16_t*)addr = data;
+	flash_wait_busy();
+	FLASH->CR &= ~FLASH_CR_PG;
+
+	if(FLASH->SR & FLASH_SR_PGERR){
+		FLASH->SR = FLASH_SR_PGERR;
+		return FLASH_ERR_PROG;
+	}
+	return FLASH_OK;
+}
+
+
